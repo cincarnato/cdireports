2d17c2eaed41bc97aecd7d975c9ec72c84cfb68b
cincarnato <cristian.cdi@gmail.com>
2014-11-27 12:17:34 -0300

    asd

--- src/CdiReport/Report/Manager.php
+++ src/CdiReport/Report/Manager.php
@@ -24,63 +24,105 @@ class Manager {
         $this->yAxis = array();
     }
 
-    public function xAxisAddElement($element, $index, $value) {
-        if(key_exists($index, $this->yAxis)){
-        $this->xAxis[$index][$element] = $value;
-        }else{
+    public function xAxisAddElement($rangeType, $index, $element, $value) {
+        if (key_exists($index, $this->yAxis)) {
+            $this->xAxis[$rangeType][$index][$element] = $value;
+        } else {
             throw new Exception("The Key no exist", "10");
         }
     }
 
-    public function yAxisAddElement($index, $value) {
-        $this->yAxis[$index] = $value;
-    }
-    
-    public function processLoadYaxisByDate(\DateTime $startDate, \DateTime $endDate){
-
+    public function yAxisAddElement($rangeType, $index, $value) {
+        $this->yAxis[$rangeType][$index] = $value;
     }
 
-    public function processObjectCollectionSet($element, $collection, $index, $value) {
+    public function processLoadYaxisByInteger($startInt, $endInt, $range = 1) {
+        if ($startInt < $endInt) {
+            while ($startInt < $endInt) {
+                $this->yAxisAddElement("integer", $startInt, $startInt);
+                $startInt = $startInt + $range;
+            }
+        }
+    }
+
+    public function processLoadYaxisByDate(\DateTime $startDate, \DateTime $endDate, $format = "Y-m-d", $rangeType = "day") {
+
+        if ($startDate < $endDate) {
+            while ($startDate < $endDate) {
+
+                $this->yAxisAddElement($rangeType, $startDate->format($format), $startDate->format($format));
+
+
+                switch ($rangeType) {
+                    case "hour":
+                        $startDate->modify("+1 hour");
+                        break;
+                    case "day":
+                        $startDate->modify("+1 day");
+                        break;
+                    case "week":
+                        $startDate->modify("+1 week");
+                        break;
+                    case "month":
+                        $startDate->modify("+1 month");
+                        break;
+
+                    default:
+                        $startDate->modify("+1 day");
+                        break;
+                }
+            }
+        }
+    }
+
+    public function processObjectCollectionSet($element, $collection, $index, $value, $rangeType = 'day') {
         $methodIndex = "get" . ucfirst($index);
         $methodValue = "get" . ucfirst($value);
         $count = array();
-        foreach ($collection as $objetc) {
-            if (method_exists($objetc, $methodIndex) && method_exists($objetc, $methodValue)) {
-                $y = $objetc->$methodIndex();
-                $x = $objetc->$methodValue();
+        foreach ($collection as $object) {
+            if (method_exists($object, $methodIndex) && method_exists($object, $methodValue)) {
+
+                $y = $this->processReturnY($object->$methodIndex(), $rangeType);
+
+
+                $x = $object->$methodValue();
                 $count[$y] = $x;
             }
         }
 
         foreach ($count as $key => $val) {
-            $this->xAxisAddElement($element, $key, $val);
+            $this->xAxisAddElement($rangeType, $key, $element, $val);
         }
     }
 
-    public function processObjectCollectionCount($element, $collection, $index) {
+    public function processObjectCollectionCount($element, $collection, $index, $rangeType = 'day') {
         $methodIndex = "get" . ucfirst($index);
         $count = array();
-        foreach ($collection as $objetc) {
-            if (method_exists($objetc, $methodIndex)) {
-                $y = $objetc->$methodIndex();
+        foreach ($collection as $object) {
+            if (method_exists($object, $methodIndex)) {
+
+                $y = $this->processReturnY($object->$methodIndex(), $rangeType);
+
+
                 $count[$y]++;
             }
         }
 
         foreach ($count as $key => $val) {
-            $this->xAxisAddElement($element, $key, $val);
+            $this->xAxisAddElement($rangeType, $key, $element, $val);
         }
     }
 
-    public function processObjectCollectionCountByCriteria($element, $collection, $index, $value, $comparedOperator = "==", $comparedVariable = null) {
+    public function processObjectCollectionCountByCriteria($element, $collection, $index, $value, $comparedOperator = "==", $comparedVariable = null, $rangeType = 'day') {
         $methodIndex = "get" . ucfirst($index);
         $methodValue = "get" . ucfirst($value);
         $count = array();
-        foreach ($collection as $objetc) {
+        foreach ($collection as $object) {
 
-            if (method_exists($objetc, $methodIndex) && method_exists($objetc, $methodValue)) {
-                $y = $objetc->$methodIndex();
-                $comparedValue = $objetc->$methodValue();
+            if (method_exists($object, $methodIndex) && method_exists($object, $methodValue)) {
+                $y = $this->processReturnY($object->$methodIndex(), $rangeType);
+
+                $comparedValue = $object->$methodValue();
 
 
                 switch ($comparedOperator) {
@@ -115,8 +157,51 @@ class Manager {
         }
 
         foreach ($count as $key => $value) {
-            $this->xAxisAddElement($element, $key, $val);
+            $this->xAxisAddElement($rangeType, $key, $element, $val);
         }
+    }
+
+    public function processReturnY($valueY, $rangeType) {
+        switch ($rangeType) {
+            case "integer":
+                $y = $valueY;
+                if (!is_a($y, "integer")) {
+                    throw new Exception("Index must be Integer");
+                }
+
+                break;
+            case "string":
+                $y = $valueY;
+                if (!is_a($y, "string")) {
+                    throw new Exception("Index must be String");
+                }
+
+                break;
+            case "day":
+                $yDt = $valueY;
+                if (is_a($y, "DateTime")) {
+                    $y = $yDt->format("Y-m-d");
+                } else {
+                    throw new Exception("Index must be Datetime");
+                }
+
+                break;
+            case "month":
+                $yDt = $valueY;
+                if (is_a($y, "DateTime")) {
+                    $y = $yDt->format("Y-m");
+                } else {
+                    throw new Exception("Index must be Datetime");
+                }
+
+                break;
+
+            default:
+                throw new Exception("rangeType must be defined");
+                break;
+        }
+
+        return $y;
     }
 
 }
